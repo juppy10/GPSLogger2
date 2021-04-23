41665d33165310d6b23b65511ca1537646ec8312
juppy10 <william.jupp@gmail.com>
2021-04-23 20:26:43 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GPSLogger2.elf.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Core/Startup/startup_stm32f302r8tx.s.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj"
+  "CMakeFiles/GPSLogger2.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj"
+  "GPSLogger2.elf"
+  "GPSLogger2.elf.manifest"
+  "GPSLogger2.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/GPSLogger2.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
